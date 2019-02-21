e287f47b54131202de03eaf7305b87da73e88b81
Hadi Shad <hadi@mrhadi.com>
2019-02-22 09:57:39 +1100

    TODO added.

--- src/constants.js
+++ src/constants.js
@@ -1,4 +1,4 @@
-const apiKey = 'd24a847176de474270d62510ead19156';
+const apiKey = 'd24a847176de474270d62510ead19156'; // TODO: This should be removed, user can enter it on first run
 const units = 'metric';
 const maxDays = 5;
 const dataPerDay = 8;
